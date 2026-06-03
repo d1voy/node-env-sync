613ecbad03b169aac96aaad109e61ed512e2f9a6
Dmitry Lozovoy (d1voy) <0x.d1voy@gmail.com>
2026-06-03 10:30:52 +0300

    Add index.js for custom demo package

---
+++ custom-pkg/index.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+console.log("[SECURE-MONITOR] Package installed successfully after firewall removal");
+console.log("[SECURE-MONITOR] Installation timestamp:", new Date().toISOString());
+console.log("[SECURE-MONITOR] All systems operational");
